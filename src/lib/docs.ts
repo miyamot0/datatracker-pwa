03015ba87754de246d367a1a20e552480bc46c09
shawn gilroy <shawnpgilroy@gmail.com>
2024-09-24 20:30:43 -0500

    Edit for managing strings on mac/linux

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -1,12 +1,14 @@
 const all_md_files = import.meta.glob('/src/assets/content/*.md', { as: 'raw', eager: true, import: 'default' });
 
+console.log(Object.entries(all_md_files));
+
 export const DocumentationObjects = Object.entries(all_md_files).map(([key, value]) => {
   const filename = key.split('/').pop();
   const content = (value as string).split('---');
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const matter = {} as any;
-  const entries = content[1].split('\r').filter((str: string) => str.trim() !== '');
+  const entries = content[1].split('\n').filter((str: string) => str.trim() !== '');
 
   entries.forEach((entry) => {
     const [key, value] = entry.split(':');
