181d22e1d7cea2cc3dec64c897a363b4381e793a
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-09-27 10:11:47 -0500

    Fix styling for key dialog

--- src/components/pages/editor-keysets/dialogs/frequency-dialog.tsx
+++ src/components/pages/editor-keysets/dialogs/frequency-dialog.tsx
@@ -1,27 +1,17 @@
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import ToolTipWrapper from "@/components/ui/tooltip-wrapper";
-import { FolderHandleContext } from "@/context/folder-context";
-import {
-  DEFAULT_ENTRY,
-  is_key_already_assigned,
-  PROTECTED_KEY_ENTRIES,
-} from "@/lib/keys";
-import { displayConditionalNotification } from "@/lib/notifications";
-import { KeySet, KeySetInstance } from "@/types/keyset";
-import { PlusIcon } from "lucide-react";
-import { createRef, useContext, useState } from "react";
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import ToolTipWrapper from '@/components/ui/tooltip-wrapper';
+import { FolderHandleContext } from '@/context/folder-context';
+import { DEFAULT_ENTRY, is_key_already_assigned, PROTECTED_KEY_ENTRIES } from '@/lib/keys';
+import { displayConditionalNotification } from '@/lib/notifications';
+import { KeySet, KeySetInstance } from '@/types/keyset';
+import { PlusIcon } from 'lucide-react';
+import { createRef, useContext, useState } from 'react';
 
 type Props = {
   KeySet: KeySet;
-  Callback: (key: KeySetInstance, type: "Duration" | "Frequency") => void;
+  Callback: (key: KeySetInstance, type: 'Duration' | 'Frequency') => void;
 };
 
 export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
@@ -40,24 +30,18 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
     >
       <div>
         <ToolTipWrapper Label="Add a new Frequency Key">
-          <Button
-            className="w-fit shadow"
-            variant={"outline"}
-            onClick={() => setShow(true)}
-          >
+          <Button className="w-fit shadow" variant={'outline'} onClick={() => setShow(true)}>
             <PlusIcon className="w-4 h-4 mr-2" /> Add Key
           </Button>
         </ToolTipWrapper>
       </div>
-      <DialogContent>
+      <DialogContent className="bg-card">
         <DialogHeader>
           <DialogTitle>Frequency Key Creator</DialogTitle>
-          <DialogDescription>
-            Set key and relevant description
-          </DialogDescription>
+          <DialogDescription>Set key and relevant description</DialogDescription>
         </DialogHeader>
         <div className="flex flex-col gap-1.5">
-          <p>Behavioral Target (e.g., SR):</p>
+          <p>Event to Record (e.g., Physical Aggression):</p>
           <Input
             className="mb-2"
             value={keyInstance.KeyDescription}
@@ -69,7 +53,7 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
             }}
           />
 
-          <p>Key Capture (Note: Press key to assign):</p>
+          <p>Key Capture (Note: Select and Press Key to Assign):</p>
           <Input
             className="mb-2"
             value={keyInstance.KeyName}
@@ -80,7 +64,7 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
 
                 displayConditionalNotification(
                   settings,
-                  "Shift/Ctrl Keys Disabled",
+                  'Shift/Ctrl Keys Disabled',
                   `You cannot use the '${event.key}' key (i.e., no capitalization or alt. characters).`,
                   3000,
                   true
@@ -93,7 +77,7 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
 
                 displayConditionalNotification(
                   settings,
-                  "Key is already assigned",
+                  'Key is already assigned',
                   `You cannot use the '${event.key}' key because it is already assigned.`,
                   3000,
                   true
@@ -101,20 +85,20 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
                 return;
               }
 
-              if (event.key === "Enter") {
+              if (event.key === 'Enter') {
                 buttonRef.current?.click();
 
                 return;
               }
 
-              if (event.key === "Escape") return;
+              if (event.key === 'Escape') return;
 
               if (PROTECTED_KEY_ENTRIES.includes(event.key)) {
                 event.preventDefault();
 
                 displayConditionalNotification(
                   settings,
-                  "Key is protected",
+                  'Key is protected',
                   `You cannot use the '${event.key}' key in a keyset because that key is reserved.`,
                   3000,
                   true
@@ -137,8 +121,8 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
             if (keyInstance.KeyDescription.trim().length < 2) {
               displayConditionalNotification(
                 settings,
-                "Key Description Too Short",
-                "The key description must be at least 2 characters long.",
+                'Key Description Too Short',
+                'The key description must be at least 2 characters long.',
                 3000,
                 true
               );
@@ -147,15 +131,15 @@ export default function FrequencyDialogKeyCreator({ KeySet, Callback }: Props) {
             if (keyInstance.KeyName.trim().length < 1) {
               displayConditionalNotification(
                 settings,
-                "The Key Captured is Invalid",
-                "The key name must be at least 1 character long.",
+                'The Key Captured is Invalid',
+                'The key name must be at least 1 character long.',
                 3000,
                 true
               );
               return;
             }
 
-            Callback(keyInstance, "Frequency");
+            Callback(keyInstance, 'Frequency');
 
             setShow(false);
             setKeyInstance(DEFAULT_ENTRY);
