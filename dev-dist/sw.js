ffdb2338c17eb82c6ebb5e8cf0c62767ce751bf1
Shawn Gilroy <shawnpgilroy@gmail.com>
2024-11-20 14:24:35 -0600

    Add in updated ss

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jmdbkqi0gj8"
+    "revision": "0.a0m71pv75lg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
