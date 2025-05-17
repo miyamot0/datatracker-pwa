d5a5ad5b4f1ea5c126b3e6b128182a5108977eb8
Shawn Gilroy <shawnpgilroy@gmail.com>
2025-05-17 09:10:30 -0500

    Add in data-table for Group (smarter long-term)

--- src/components/pages/dashboard-group/displays/authorized-display.tsx
+++ src/components/pages/dashboard-group/displays/authorized-display.tsx
@@ -1,6 +1,10 @@
 import BackButton from '@/components/ui/back-button';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import ToolTipWrapper from '@/components/ui/tooltip-wrapper';
+import createHref from '@/lib/links';
+import { ChevronDown, FolderInput, FolderPlus, FolderX } from 'lucide-react';
+import { DataTable } from '../../../ui/data-table-common';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,14 +13,12 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from '@/components/ui/table';
-import ToolTipWrapper from '@/components/ui/tooltip-wrapper';
-import { FolderHandleContext } from '@/context/folder-context';
-import createHref from '@/lib/links';
-import { cn } from '@/lib/utils';
-import { ChevronDown, FolderInput, FolderPlus, FolderX } from 'lucide-react';
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { DataTableColumnHeader } from '@/components/ui/data-table-column-header';
+import { ColumnDef } from '@tanstack/react-table';
+import { FolderHandleContext } from '@/context/folder-context';
+import { useContext } from 'react';
+import { cn } from '@/lib/utils';
 
 type Props = {
   Groups: string[];
@@ -24,8 +26,63 @@ type Props = {
   RemoveGroup: (group: string) => void;
 };
 
+type GroupTableRow = {
+  Group: string;
+};
+
 export default function AuthorizedDisplay({ Groups, AddGroup, RemoveGroup }: Props) {
   const { settings } = useContext(FolderHandleContext);
+
+  const columns: ColumnDef<GroupTableRow>[] = [
+    {
+      accessorKey: 'Group',
+      header: ({ column }) => <DataTableColumnHeader column={column} title="Client Group Folder" />,
+    },
+    {
+      accessorKey: 'Actions',
+
+      header: () => <div className="text-right">Group Folder Actions</div>,
+      cell: ({ row }) => (
+        <div className="flex flex-row justify-end">
+          <Button size={'sm'} variant={'outline'} className="flex flex-row divide-x justify-between mx-0 px-0 shadow">
+            <Link
+              unstable_viewTransition
+              className="px-3 hover:underline flex flex-row items-center"
+              to={createHref({ type: 'Individuals', group: row.original.Group })}
+            >
+              <FolderInput className="mr-2 h-4 w-4" />
+              Open Group
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <ChevronDown className="w-fit px-2" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" side="bottom" align="end" sideOffset={12}>
+                <DropdownMenuLabel>Data Management</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className={cn(
+                    'bg-red-500 text-white hover:bg-red-400 focus:bg-red-400 focus:text-white rounded cursor-pointer',
+                    {
+                      disabled: settings.EnableFileDeletion === false,
+                      'pointer-events-none': settings.EnableFileDeletion === false,
+                    }
+                  )}
+                  disabled={settings.EnableFileDeletion === false}
+                  onClick={async () => {
+                    await RemoveGroup(row.original.Group);
+                  }}
+                >
+                  <FolderX className="mr-2 h-4 w-4" />
+                  Delete Group
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </Button>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <Card className="w-full max-w-screen-2xl">
@@ -61,62 +118,13 @@ export default function AuthorizedDisplay({ Groups, AddGroup, RemoveGroup }: Pro
           at least <i>one</i> group to begin collecting data.
         </p>
 
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Client Group Folder</TableHead>
-              <TableHead className="text-right">Group Folder Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {Groups.map((group, index) => (
-              <TableRow key={index} className="my-2">
-                <TableCell>{group}</TableCell>
-                <TableCell className="flex flex-row justify-end">
-                  <Button
-                    size={'sm'}
-                    variant={'outline'}
-                    className="flex flex-row divide-x justify-between mx-0 px-0 shadow"
-                  >
-                    <Link
-                      unstable_viewTransition
-                      className="px-3 hover:underline flex flex-row items-center"
-                      to={createHref({ type: 'Individuals', group })}
-                    >
-                      <FolderInput className="mr-2 h-4 w-4" />
-                      Open Group
-                    </Link>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <ChevronDown className="w-fit px-2" />
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent className="w-56" side="bottom" align="end" sideOffset={12}>
-                        <DropdownMenuLabel>Data Management</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem
-                          className={cn(
-                            'bg-red-500 text-white hover:bg-red-400 focus:bg-red-400 focus:text-white rounded cursor-pointer',
-                            {
-                              disabled: settings.EnableFileDeletion === false,
-                              'pointer-events-none': settings.EnableFileDeletion === false,
-                            }
-                          )}
-                          disabled={settings.EnableFileDeletion === false}
-                          onClick={async () => {
-                            await RemoveGroup(group);
-                          }}
-                        >
-                          <FolderX className="mr-2 h-4 w-4" />
-                          Delete Group
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <DataTable
+          columns={columns}
+          data={Groups.map((g) => {
+            return { Group: g };
+          })}
+          filterCol="Group"
+        />
       </CardContent>
     </Card>
   );
---
+++ src/components/ui/data-table-common.tsx
@@ -0,0 +1,100 @@
+import { DataTablePagination } from '@/components/ui/data-table-pagination';
+import { Input } from '@/components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { FolderHandleContext } from '@/context/folder-context';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from '@tanstack/react-table';
+import { useContext, useState } from 'react';
+
+export type RowSelectOptions = 'None';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  filterCol: string;
+  rowSelectOptions?: RowSelectOptions;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  filterCol,
+  rowSelectOptions = 'None',
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
+  return (
+    <div className="">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder={`Filter by ${filterCol}`}
+          value={table.getColumn(filterCol)?.getFilterValue() as string}
+          onChange={(event) => table.getColumn(filterCol)?.setFilterValue(event.target.value)}
+          className="max-w-sm"
+        />
+      </div>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <DataTablePagination table={table} rowSelectOptions="None" />
+    </div>
+  );
+}
