c6f9e4fc598e07967bc053bf65ef8b441a9fa8cf
Shawn Gilroy <shawnpgilroy@gmail.com>
2025-06-04 12:44:53 -0500

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 import package_json from './package.json';
-import { analyzer } from 'vite-bundle-analyzer';
+//import { analyzer } from 'vite-bundle-analyzer';
 
 const common_screenshot_params = {
   sizes: '1148x969',
