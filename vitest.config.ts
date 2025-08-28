2298fe1997babda855efd0f81056968e3b324389
Shawn Gilroy <shawnpgilroy@gmail.com>
2025-08-28 15:48:29 -0500

    Update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['./src/lib/**/*.test.{ts,tsx}'],
+    reporters: ['default'],
     environment: 'jsdom',
     globals: true,
     coverage: {
+      provider: 'v8',
       reporter: ['json', 'lcov', 'text', 'clover', 'json-summary'],
       reportsDirectory: './coverage',
       include: ['src/lib/**/*.{ts,tsx}'],
