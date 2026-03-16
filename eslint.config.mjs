fcd1e47b0061558d57757abf07046895a5ae87dd
Shawn Gilroy <shawnpgilroy@gmail.com>
2026-03-16 11:07:52 -0500

    Add eslint checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,34 @@
 import eslint from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  globalIgnores(['dist', 'dev-dist', 'coverage', 'node_modules', '**/*.js', 'public', 'scripts', '**/*.test.{ts,tsx}']),
+  jsxA11y.flatConfigs.recommended,
+  globalIgnores([
+    'dist',
+    'dev-dist',
+    'coverage',
+    'node_modules',
+    '**/*.js',
+    'public',
+    'scripts',
+    '**/*.test.{ts,tsx}',
+    '.eslintrc.{js,json}',
+    'eslint.config.{js,json}',
+    'prettier.rc',
+    'components.json',
+    'tailwind.config.{js,json}',
+    'tsconfig.{json,ts}',
+    'tsconfig.app.{json,ts}',
+    'tsconfig.node.{json,ts}',
+    'vercel.json',
+    'vite.config.{js,ts}',
+    'vitest.config.{js,ts}',
+    'yarn.lock',
+  ]),
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
