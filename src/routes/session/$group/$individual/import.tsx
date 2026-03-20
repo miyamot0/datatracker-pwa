0894040f4f524844d85f262003fe96905ff8a1cc
Shawn Gilroy <shawnpgilroy@gmail.com>
2026-03-20 15:15:48 -0500

    Eval routes

--- src/routes/session/$group/$individual/import.tsx
+++ src/routes/session/$group/$individual/import.tsx
@@ -1,6 +1,15 @@
+import PageWrapper from '@/components/elements/page-wrapper';
 import ViewerEvaluationsPage from '@/components/pages/dashboard-evaluations-import/viewer-evaluations-page';
+import { LoadingDisplay } from '@/components/suspense/loading-display';
+import {
+  BuildEvaluationsBreadcrumb,
+  BuildGroupBreadcrumb,
+  BuildIndividualsBreadcrumb,
+} from '@/components/ui/breadcrumb-entries';
 import { CleanUpString } from '@/lib/strings';
-import { createFileRoute, redirect } from '@tanstack/react-router';
+import { evaluationsAllQueryOptions } from '@/queries/evaluations/query-evaluations-all';
+import { EvaluationRecord } from '@/queries/keysets/types/evaluation-record';
+import { Await, createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/session/$group/$individual/import')({
   beforeLoad: ({ context, params }) => {
@@ -23,20 +32,54 @@ export const Route = createFileRoute('/session/$group/$individual/import')({
         to: '/dashboard',
       });
     }
-  },
-  loader: ({ params }) => {
-    const { group, individual } = params;
 
     return {
       Group: CleanUpString(group),
       Individual: CleanUpString(individual),
+      CleanHandle: context.folderHandleContext.handle,
+    };
+  },
+  loader: ({ context }) => {
+    const { Group, Individual, CleanHandle } = context;
+
+    const fetchAllEvaluation = context.queryClient.fetchQuery(evaluationsAllQueryOptions(CleanHandle));
+
+    return {
+      Group: CleanUpString(Group),
+      Individual: CleanUpString(Individual),
+      Settings: context.folderHandleContext.settings,
+      CleanHandle,
+      fetchAllEvaluation,
     };
   },
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { Group, Individual } = Route.useLoaderData();
+  const { Group, Individual, fetchAllEvaluation, CleanHandle, Settings } = Route.useLoaderData();
 
-  return <ViewerEvaluationsPage Group={Group} Individual={Individual} />;
+  return (
+    <PageWrapper
+      breadcrumbs={[
+        BuildGroupBreadcrumb(),
+        BuildIndividualsBreadcrumb(Group),
+        BuildEvaluationsBreadcrumb(Group, Individual),
+      ]}
+      label={'Evaluation Import'}
+      className="select-none"
+      Settings={Settings}
+    >
+      <Await promise={fetchAllEvaluation} fallback={<LoadingDisplay />}>
+        {(evaluations: EvaluationRecord[]) => (
+          <ViewerEvaluationsPage
+            Group={Group}
+            Individual={Individual}
+            Handle={CleanHandle}
+            Settings={Settings}
+            Evaluations={evaluations}
+          />
+        )}
+      </Await>
+    </PageWrapper>
+  );
 }
