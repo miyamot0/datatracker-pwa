4753c9178b5d6a1f76c3250cdac1b6608f954986
Shawn Gilroy <shawnpgilroy@gmail.com>
2026-03-24 11:14:50 -0500

    add tests.com

---
+++ src/workers/__tests__/session-recorder-worker-actual.test.ts
@@ -0,0 +1,12 @@
+// @vitest-environment node
+
+import '@vitest/web-worker';
+import SessionRecorderWorker from '../timing/session-recorder-worker?worker';
+
+describe('SessionRecorderWorker actual', () => {
+  const worker = new SessionRecorderWorker();
+
+  it('should start', () => {
+    expect(1).toBe(1);
+  });
+});
