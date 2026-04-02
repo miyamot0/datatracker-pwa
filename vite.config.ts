34028c87430f7b04b846fa7c89eb095f68989baf
Shawn Gilroy <shawnpgilroy@gmail.com>
2026-04-02 18:17:36 -0500

    Respect order of build for hash

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,61 @@ type Modality = 'base' | 'island';
 function PluginSetup(plugins: PluginOption[], approach: Modality) {
   switch (approach) {
     case 'base':
+      plugins.push({
+        name: 'convert-base-png-docs',
+        async writeBundle() {
+          // Pull relevant PNGs
+          const pngFiles = await fs.readdir(`${PROD_OUT_DIR}/docs`);
+
+          const prePng = performance.now();
+          console.log(`Found ${pngFiles.length} PNG files in docs, starting optimization and conversion...`);
+
+          for (const file of pngFiles) {
+            if (file.endsWith('.png')) {
+              // Convert old
+              await imagemin([`${PROD_OUT_DIR}/docs/${file}`], {
+                destination: `${PROD_OUT_DIR}/docs`,
+                plugins: [imageminWebp({ quality: 75 })],
+              });
+
+              // Trash old
+              await fs.rm(`${PROD_OUT_DIR}/docs/${file}`);
+            }
+          }
+
+          const postPng = performance.now();
+          console.log('Docs images optimized and converted!', ((postPng - prePng) / 1000).toFixed(2), 'seconds');
+          console.log();
+
+          /*
+          Note: This wasn't really worth the complexity TBH
+          // Pull relevant MDs
+          const mdFiles = await fs.readdir(`${PROD_OUT_DIR}/content`);
+
+          console.log(`Found ${mdFiles.length} Markdown files, starting update...`);
+          const preMd = performance.now();
+
+          for (const file of mdFiles) {
+            if (file.endsWith('.md')) {
+              const filePath = `${PROD_OUT_DIR}/content/${file}`;
+              let content = await fs.readFile(filePath, 'utf-8');
+
+              // Replace .png with .webp in markdown content
+              content = content.replaceAll('.png', '.webp');
+
+              await fs.writeFile(filePath, content, 'utf-8');
+            }
+          }
+          const postMd = performance.now();
+          console.log(
+            'Markdown files updated with new image formats!',
+            ((postMd - preMd) / 1000).toFixed(2),
+            'seconds',
+          );
+          console.log();
+          */
+        },
+      });
       plugins.push(
         VitePWA({
           registerType: 'autoUpdate',
@@ -38,6 +93,22 @@ function PluginSetup(plugins: PluginOption[], approach: Modality) {
             sourcemap: false,
             maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
             runtimeCaching: [
+              {
+                urlPattern: ({ request }) => request.destination === 'document',
+                handler: 'NetworkFirst',
+              },
+              {
+                urlPattern: ({ request }) => request.destination === 'script',
+                handler: 'CacheFirst',
+              },
+              {
+                urlPattern: ({ request }) => request.destination === 'style',
+                handler: 'CacheFirst',
+              },
+              {
+                urlPattern: ({ request }) => request.destination === 'image',
+                handler: 'CacheFirst',
+              },
               {
                 urlPattern: /https:\/\/www\.google-analytics\.com/,
                 handler: 'NetworkOnly',
@@ -144,61 +215,6 @@ function PluginSetup(plugins: PluginOption[], approach: Modality) {
           },
         }),
       );
-      plugins.push({
-        name: 'convert-base-png-docs',
-        async writeBundle() {
-          // Pull relevant PNGs
-          const pngFiles = await fs.readdir(`${PROD_OUT_DIR}/docs`);
-
-          const prePng = performance.now();
-          console.log(`Found ${pngFiles.length} PNG files in docs, starting optimization and conversion...`);
-
-          for (const file of pngFiles) {
-            if (file.endsWith('.png')) {
-              // Convert old
-              await imagemin([`${PROD_OUT_DIR}/docs/${file}`], {
-                destination: `${PROD_OUT_DIR}/docs`,
-                plugins: [imageminWebp({ quality: 75 })],
-              });
-
-              // Trash old
-              await fs.rm(`${PROD_OUT_DIR}/docs/${file}`);
-            }
-          }
-
-          const postPng = performance.now();
-          console.log('Docs images optimized and converted!', ((postPng - prePng) / 1000).toFixed(2), 'seconds');
-          console.log();
-
-          /*
-          Note: This wasn't really worth the complexity TBH
-          // Pull relevant MDs
-          const mdFiles = await fs.readdir(`${PROD_OUT_DIR}/content`);
-
-          console.log(`Found ${mdFiles.length} Markdown files, starting update...`);
-          const preMd = performance.now();
-
-          for (const file of mdFiles) {
-            if (file.endsWith('.md')) {
-              const filePath = `${PROD_OUT_DIR}/content/${file}`;
-              let content = await fs.readFile(filePath, 'utf-8');
-
-              // Replace .png with .webp in markdown content
-              content = content.replaceAll('.png', '.webp');
-
-              await fs.writeFile(filePath, content, 'utf-8');
-            }
-          }
-          const postMd = performance.now();
-          console.log(
-            'Markdown files updated with new image formats!',
-            ((postMd - preMd) / 1000).toFixed(2),
-            'seconds',
-          );
-          console.log();
-          */
-        },
-      });
       break;
 
     default:
