52126c39d9a080c0362b903f80938aba162d5f67
Shawn Gilroy <=>
2026-04-03 11:42:40 -0500

    Cleanup of config

---
+++ scripts/plugins/delete-unnecessary-files.ts
@@ -0,0 +1,16 @@
+import { PluginOption } from 'vite';
+import fs from 'node:fs/promises';
+import { ISLAND_OUT_DIR } from '../../vite.config';
+
+export const deleteUnnecessaryPlugin: PluginOption = {
+  name: 'delete-unnecessary',
+  async writeBundle() {
+    await fs.rm(`${ISLAND_OUT_DIR}/screenshots`, { recursive: true });
+    await fs.rm(`${ISLAND_OUT_DIR}/manifest.json`);
+    await fs.rm(`${ISLAND_OUT_DIR}/robots.txt`);
+
+    (await fs.readdir(ISLAND_OUT_DIR))
+      .filter((f) => f.endsWith('.png'))
+      .forEach((f) => fs.rm(`${ISLAND_OUT_DIR}/${f}`));
+  },
+};
